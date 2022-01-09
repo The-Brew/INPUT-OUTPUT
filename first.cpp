e2fa8367e47a40f05403dae0a8c7702ce01f63b9
mohit <themohit002@gmail.com>
2022-01-10 03:12:04 +0530

    first commit

---
+++ first.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int a=5,b=7 ,sum;
+
+sum= a+b;
+cout<< "the sum is"<< sum;
+return 0;
+}
---
+++ sec.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{    
+    int divisor, dividend, quotient, remainder;
+
+    cout << "Enter dividend: ";
+    cin >> dividend;
+
+    cout << "Enter divisor: ";
+    cin >> divisor;
+
+    quotient = dividend / divisor;
+    remainder = dividend % divisor;
+
+    cout << "Quotient = " << quotient << endl;
+    cout << "Remainder = " << remainder;
+
+    return 0;
+}
