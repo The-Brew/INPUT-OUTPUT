6a5df23b745b2a7c17bf0c74d2f5fdee4816347e
mohit <themohit002@gmail.com>
2022-01-12 00:06:35 +0530

    first commit

---
+++ Ques10.cpp
@@ -0,0 +1,14 @@
+/*wap to print ASCII  code of a given digit*/
+#include<bits/stdc++.h>
+using namespace std;
+ int main()
+ {
+     char c= 'a'; //first char//
+     char b= 'A';  // second char//
+     char a='D';
+     cout<<(int)a-b-c<<endl; //here is typecasting a char value as an integer//
+   cout<< b-c<<endl;
+     
+
+     return 0;
+ }
