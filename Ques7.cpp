c8992638442cdecb877c0df604f519e4aed64884
mohit <themohit002@gmail.com>
2022-01-11 21:33:24 +0530

    first commit

---
+++ Ques7.cpp
@@ -0,0 +1,16 @@
+//wap to calculate the number of HCF of a number. if LCM  and product of a number are given//
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int LCM, HCF, PRO_OF_NUM;
+    cout<<"\n enter lcm::";
+    cin>>LCM;
+    cout<<"\n enter pro_of_num::";
+    cin>>PRO_OF_NUM;
+ HCF=PRO_OF_NUM/LCM;
+ cout<<"\n enter hcf of a num::"<<HCF;
+ return 0;
+
+}
