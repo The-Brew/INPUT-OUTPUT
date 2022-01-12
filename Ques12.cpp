104b4fd2bbccc3927aa58ec1ff97f6f84a09ea81
mohit <themohit002@gmail.com>
2022-01-13 00:38:19 +0530

    first commit

---
+++ Ques12.cpp
@@ -0,0 +1,14 @@
+/* Wap to find the result of the expression x^2+4x+4for a given
+ value of x. */
+
+#include<bits/stdc++.h>
+using namespace std;
+ int main()
+ {
+     int num,x;
+     cout<<"\n enter the number::";
+     cin>>num;
+x=(num*num+4*num+4);
+     cout<<x<<endl;
+     return 0;
+ }
