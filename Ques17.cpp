21c2a09087cfcf1cdcd776fcfb935cd084af23a1
mohit <themohit002@gmail.com>
2022-01-13 02:03:08 +0530

    first commit

---
+++ Ques17.cpp
@@ -0,0 +1,14 @@
+// wap to find area of triangle.
+#include<bits/stdc++.h>
+using namespace std;
+ int main()
+ {
+     int base, height, area;
+     cout<<"\n enter base ::";
+     cin>>base;
+     cout<<"\n enter height ::";
+     cin>>height;
+      area = (base*height)/2;
+      cout<< area<< endl;
+      return 0;
+ }
