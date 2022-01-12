6a694dda93dd19d5a4425f43c0a29e91debe9a73
mohit <themohit002@gmail.com>
2022-01-13 00:59:28 +0530

    first commit

---
+++ Ques13.cpp
@@ -0,0 +1,29 @@
+/* wap to calculate the values of the expression (m*n)/(x+y)
+for given value of m,n,x and y. */
+#include<bits/stdc++.h>
+using namespace std;
+ int main()
+ {
+     int m, n, x, y, exp;
+      cout<<"\n  enter m ::";
+     cin>>m;
+
+     cout<<"\n  enter n ::";
+     cin>>n;
+
+   cout<<"\n  enter x ::";
+     cin>>x;
+
+     cout<<"\n  enter y ::";
+     cin>>y;
+
+    
+     exp= (m*n)/(x+y);
+     cout<< exp<< endl;
+
+     
+     return 0;
+
+
+
+ }
