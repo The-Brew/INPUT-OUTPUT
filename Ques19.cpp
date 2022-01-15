29b18524b3bacd98255aa3df0a6bc8ff1fe1b80d
mohit <themohit002@gmail.com>
2022-01-15 22:07:08 +0530

    first commit

---
+++ Ques19.cpp
@@ -0,0 +1,18 @@
+// wap to find the area in which cow gaazes.If it is tied on a rope of given lenght//
+#include<bits/stdc++.h>
+using namespace std;
+ int main()
+ {
+     int meter;
+     int radius= meter;
+     
+     
+     float area;
+     cout<<"\n lenght of the rope as meter ::"; // here lenght is considered as radius of the rope//
+     cin>> radius;
+     area= 3.14*(radius*radius)/4;
+     cout<<"\n area ::"<<area;
+     return 0;
+
+
+ }
